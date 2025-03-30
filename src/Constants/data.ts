dd8f4dbb8060c7d4a662d661047f38a6b5d1d3bf
learnwithjacksun <codewithjacksun@gmail.com>
2025-03-30 23:53:31 +0100

    Installed libraries and a home screen 🌟

---
+++ src/Constants/data.ts
@@ -0,0 +1,12 @@
+export const libraries = [
+    "Zustand",
+    "React Router",
+    "React Query",
+    "React Hook Form",
+    "Sonner",
+    "Lucide-React",
+    "Tailwind CSS",
+    "TypeScript",
+    "axios",
+    "Framer Motion",
+]
